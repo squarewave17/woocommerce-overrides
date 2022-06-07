26ace9958573aef13d5231cd1cab21c3bcbe92e5
squarewave17 <paul@squarewavedigital.co.uk>
2022-06-07 13:39:18 +0100

    fix file path

--- templates/myaccount/view-order.php
+++ templates/myaccount/view-order.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * View Order
  *
@@ -17,40 +18,44 @@
  * @version 3.0.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 $notes = $order->get_customer_order_notes();
 ?>
 <p>
-<?php
-printf(
-	/* translators: 1: order number 2: order date 3: order status */
-	esc_html__( 'Order #%1$s was placed on %2$s and is currently %3$s.', 'woocommerce' ),
-	'<mark class="order-number">' . $order->get_order_number() . '</mark>', // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	'<mark class="order-date">' . wc_format_datetime( $order->get_date_created() ) . '</mark>', // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	'<mark class="order-status">' . wc_get_order_status_name( $order->get_status() ) . '</mark>' // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-);
-?>
+	<?php
+	printf(
+		/* translators: 1: order number 2: order date 3: order status */
+		esc_html__('Order #%1$s was placed on %2$s and is currently %3$s.', 'woocommerce'),
+		'<mark class="order-number">' . $order->get_order_number() . '</mark>', // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		'<mark class="order-date">' . wc_format_datetime($order->get_date_created()) . '</mark>', // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		'<mark class="order-status">' . wc_get_order_status_name($order->get_status()) . '</mark>' // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	);
+	?>
 </p>
 
-<?php if ( $notes ) : ?>
-	<h2><?php esc_html_e( 'Order updates', 'woocommerce' ); ?></h2>
+<?php if ($notes) : ?>
+	<h2><?php esc_html_e('Order updates', 'woocommerce'); ?></h2>
 	<ol class="woocommerce-OrderUpdates commentlist notes">
-		<?php foreach ( $notes as $note ) : ?>
-		<li class="woocommerce-OrderUpdate comment note">
-			<div class="woocommerce-OrderUpdate-inner comment_container">
-				<div class="woocommerce-OrderUpdate-text comment-text">
-					<p class="woocommerce-OrderUpdate-meta meta"><?php echo date_i18n( esc_html__( 'l jS \o\f F Y, h:ia', 'woocommerce' ), strtotime( $note->comment_date ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-					<div class="woocommerce-OrderUpdate-description description">
-						<?php echo wpautop( wptexturize( $note->comment_content ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+		<?php foreach ($notes as $note) : ?>
+			<li class="woocommerce-OrderUpdate comment note">
+				<div class="woocommerce-OrderUpdate-inner comment_container">
+					<div class="woocommerce-OrderUpdate-text comment-text">
+						<p class="woocommerce-OrderUpdate-meta meta"><?php echo date_i18n(esc_html__('l jS \o\f F Y, h:ia', 'woocommerce'), strtotime($note->comment_date)); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+																		?></p>
+						<div class="woocommerce-OrderUpdate-description description">
+							<?php echo wpautop(wptexturize($note->comment_content)); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+							?>
+						</div>
+						<div class="clear"></div>
 					</div>
 					<div class="clear"></div>
 				</div>
-				<div class="clear"></div>
-			</div>
-		</li>
+			</li>
 		<?php endforeach; ?>
 	</ol>
 <?php endif; ?>
 
-<?php do_action( 'woocommerce_view_order', $order_id ); ?>
+<?php do_action('woocommerce_view_order', $order_id); ?>
+
+Array ( [0] => WC_Meta_Data Object ( [current_data:protected] => Array ( [id] => 685 [key] => is_vat_exempt [value] => no ) [data:protected] => Array ( [id] => 685 [key] => is_vat_exempt [value] => no ) ) [1] => WC_Meta_Data Object ( [current_data:protected] => Array ( [id] => 686 [key] => _ppcp_paypal_order_id [value] => 0BJ20582GL014351R ) [data:protected] => Array ( [id] => 686 [key] => _ppcp_paypal_order_id [value] => 0BJ20582GL014351R ) ) [2] => WC_Meta_Data Object ( [current_data:protected] => Array ( [id] => 687 [key] => _ppcp_paypal_intent [value] => CAPTURE ) [data:protected] => Array ( [id] => 687 [key] => _ppcp_paypal_intent [value] => CAPTURE ) ) [3] => WC_Meta_Data Object ( [current_data:protected] => Array ( [id] => 688 [key] => _ppcp_paypal_payment_mode [value] => live ) [data:protected] => Array ( [id] => 688 [key] => _ppcp_paypal_payment_mode [value] => live ) ) [4] => WC_Meta_Data Object ( [current_data:protected] => Array ( [id] => 698 [key] => _new_order_email_sent [value] => true ) [data:protected] => Array ( [id] => 698 [key] => _new_order_email_sent [value] => true ) ) [5] => WC_Meta_Data Object ( [current_data:protected] => Array ( [id] => 699 [key] => _el_meta [value] => a:1:{s:4:"el_2";a:1:{i:0;s:35:"747D148E-AC649B89-CD56BFE8-1D5B3C0D";}} ) [data:protected] => Array ( [id] => 699 [key] => _el_meta [value] => a:1:{s:4:"el_2";a:1:{i:0;s:35:"747D148E-AC649B89-CD56BFE8-1D5B3C0D";}} ) ) [6] => WC_Meta_Data Object ( [current_data:protected] => Array ( [id] => 700 [key] => _ppcp_paypal_fees [value] => Array ( [gross_amount] => Array ( [currency_code] => GBP [value] => 1.00 ) [paypal_fee] => Array ( [currency_code] => GBP [value] => 0.33 ) [net_amount] => Array ( [currency_code] => GBP [value] => 0.67 ) ) ) [data:protected] => Array ( [id] => 700 [key] => _ppcp_paypal_fees [value] => Array ( [gross_amount] => Array ( [currency_code] => GBP [value] => 1.00 ) [paypal_fee] => Array ( [currency_code] => GBP [value] => 0.33 ) [net_amount] => Array ( [currency_code] => GBP [value] => 0.67 ) ) ) ) [7] => WC_Meta_Data Object ( [current_data:protected] => Array ( [id] => 701 [key] => PayPal Transaction Key [value] => 0.33 ) [data:protected] => Array ( [id] => 701 [key] => PayPal Transaction Key [value] => 0.33 ) ) )
--- woocommerce-overrides.php
+++ woocommerce-overrides.php
@@ -20,8 +20,8 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+   die;
 }
 
 /**
@@ -30,41 +30,43 @@ if ( ! defined( 'WPINC' ) ) {
 
 //  ONE BY ONE
 //add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
-function jk_dequeue_styles( $enqueue_styles ) {
-	unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
-	unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
-	unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
-	unset( $enqueue_styles['select2'] );	
-	return $enqueue_styles;
+function jk_dequeue_styles($enqueue_styles)
+{
+   unset($enqueue_styles['woocommerce-general']);   // Remove the gloss
+   unset($enqueue_styles['woocommerce-layout']);      // Remove the layout
+   unset($enqueue_styles['woocommerce-smallscreen']);   // Remove the smallscreen optimisation
+   unset($enqueue_styles['select2']);
+   return $enqueue_styles;
 }
 
 // OR EVERYTHING
-add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+add_filter('woocommerce_enqueue_styles', '__return_false');
 
 /**
  * Get woocommerce to use our templates instead
  */
-add_filter( 'woocommerce_locate_template', 'woocommerce_overrides', 1, 3 );
-   function woocommerce_overrides( $template, $template_name, $template_path ) {
-     global $woocommerce;
-     $_template = $template;
-     if ( ! $template_path ) 
-        $template_path = $woocommerce->template_url;
-     $plugin_path  = untrailingslashit( plugin_dir_path( __FILE__ ) )  . '/templates/';
- 
-    // Look within passed path within the theme - this is priority
-    $template = locate_template(
-    array(
-      $template_path . $template_name,
-      $template_name
-    )
+add_filter('woocommerce_locate_template', 'woocommerce_overrides', 1, 3);
+function woocommerce_overrides($template, $template_name, $template_path)
+{
+   global $woocommerce;
+   $_template = $template;
+   if (!$template_path)
+      $template_path = $woocommerce->template_url;
+   $plugin_path  = untrailingslashit(plugin_dir_path(__FILE__))  . '/templates/';
+
+   // Look within passed path within the theme - this is priority
+   $template = locate_template(
+      array(
+         $template_path . $template_name,
+         $template_name
+      )
    );
- 
-   if( ! $template && file_exists( $plugin_path . $template_name ) )
-    $template = $plugin_path . $template_name;
- 
-   if ( ! $template )
-    $template = $_template;
+
+   if (!$template && file_exists($plugin_path . $template_name))
+      $template = $plugin_path . $template_name;
+
+   if (!$template)
+      $template = $_template;
 
    return $template;
 }
@@ -72,11 +74,12 @@ add_filter( 'woocommerce_locate_template', 'woocommerce_overrides', 1, 3 );
 /**
  * Enqueue our own stylesheet after everything else
  */
-function woo_overrides_styles(){
-	wp_register_style( 'woo-overrides', untrailingslashit( plugin_dir_path( __FILE__ ) )  . '/templates/css/woocommerce.css');
-	
-	if ( class_exists( 'woocommerce' ) ) {
-		wp_enqueue_style( 'woo-overrides' );
-	}
+function woo_overrides_styles()
+{
+   wp_register_style('woo-overrides', plugin_dir_url(__FILE__) . 'templates/css/woocommerce.css');
+
+   if (class_exists('woocommerce')) {
+      wp_enqueue_style('woo-overrides');
+   }
 }
-add_action( 'wp_enqueue_scripts', 'woo_overrides_styles', 99 );
+add_action('wp_enqueue_scripts', 'woo_overrides_styles', 99);
